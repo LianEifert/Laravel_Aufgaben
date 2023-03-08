f32c2eed251892ea4df12590ac652caa4d42e073
Lian Eifert <lian.eifert@gmx.ch>
2023-03-08 21:28:14 +0100

    Hausaufgaben fertig 🙂🙂

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Application;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/form_create', function () {
+    return view('form_create');
+});
+
+Route::get('/form_application', function () {
+    return view('form_application');
+});
+
+
+Route::post('/form_application', function () {
+    $request = request();
+
+    $application = new Application();
+    $application->firstname = $request->get('firstname');
+    $application->lastname = $request->get('lastname');
+    $application->email = $request->get('email');
+    $application->answer = $request->get('answer');
+    
+    $application->save();
+
+    return redirect('/form_application');
+
+});
+
+
+
+
+
+Route::get('/form_application/applications', function () {
+    
+    $applications = Application::where('answer', 'yes')->get();
+
+    $declinedapplications = Application::where('answer', 'no')->count();
+
+  
+
+
+
+    return view('applications', [
+'applications' => $applications,
+'declinedapplications' => $declinedapplications,
+
+    ]);
+});
+
+
+
+
+
